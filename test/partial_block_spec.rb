c6369404e1738da9ca86b82461ee65d8348b4438
aaccurso <accurso.alan@gmail.com>
2015-04-14 03:45:33 -0300

    refactor(before_all): adds before_all for tests

--- test/partial_block_spec.rb
+++ test/partial_block_spec.rb
@@ -1,13 +1,12 @@
 require 'rspec'
 require_relative '../lib/multimethods/partial_block'
 
-def before_all
-  PartialBlock.new([String]) do |who|
-    "Hello #{who}"
-  end
-end
-
 describe 'Partial Block' do
+  before(:all) do
+    @helloBlock = PartialBlock.new([String]) do |who|
+      "Hello #{who}"
+    end
+  end
   it 'should validate if block is given' do
     expect{
       PartialBlock.new([Object])
@@ -15,23 +14,17 @@ describe 'Partial Block' do
   end
 
   it 'should validate argument types' do
-    helloBlock = before_all
-
-    expect(helloBlock.matches?("a")).to be_truthy
-    expect(helloBlock.matches?(1)).to be_falsey
-    expect(helloBlock.matches?("a", "b")).to be_falsey
+    expect(@helloBlock.matches?("a")).to be_truthy
+    expect(@helloBlock.matches?(1)).to be_falsey
+    expect(@helloBlock.matches?("a", "b")).to be_falsey
   end
 
   it 'should call partial block with valid arguments' do
-    helloBlock = before_all
-
-    expect(helloBlock.call("a")).to eq("Hello a")
+    expect(@helloBlock.call("a")).to eq("Hello a")
   end
 
   it 'should validate argument types before executing partial block' do
-    helloBlock = before_all
-
-    expect{ helloBlock.call(2) }.to raise_error
-    expect{ helloBlock.call("c", 2) }.to raise_error
+    expect{ @helloBlock.call(2) }.to raise_error
+    expect{ @helloBlock.call("c", 2) }.to raise_error
   end
 end
